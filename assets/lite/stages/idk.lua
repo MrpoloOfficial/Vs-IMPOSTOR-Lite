05b5d96831df9b7b5b919e20a298738264bd8b84
NotMrPolo-real <154438885+NotMrPolo-real@users.noreply.github.com>
2025-02-10 17:17:47 +0330

    idk stage change - ty jordan

--- assets/lite/stages/idk.lua
+++ assets/lite/stages/idk.lua
@@ -1,5 +1,5 @@
 function onCreate()
-    makeLuaSprite("stage", "bg/idk/background", -1400, -600)
+    makeLuaSprite("stage", "bg/idk/background", -3000, -600)
     scaleObject("stage", 2.8, 2.8)
     setProperty("stage.antialiasing", false)
     addLuaSprite("stage")
